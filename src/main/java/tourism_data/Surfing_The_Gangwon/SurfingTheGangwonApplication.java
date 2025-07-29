8f4a838b74838da463f66efb0223ca31711898f1
unknown <kim0615157@gmail.com>
2025-07-29 10:22:42 +0900

    init commit

---
+++ src/main/java/tourism_data/Surfing_The_Gangwon/SurfingTheGangwonApplication.java
@@ -0,0 +1,13 @@
+package tourism_data.Surfing_The_Gangwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfingTheGangwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfingTheGangwonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tourism_data/Surfing_The_Gangwon/SurfingTheGangwonApplicationTests.java
@@ -0,0 +1,13 @@
+package tourism_data.Surfing_The_Gangwon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfingTheGangwonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
