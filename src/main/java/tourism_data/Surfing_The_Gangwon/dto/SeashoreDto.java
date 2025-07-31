111b77d78697a516e31eabd51d4ebb793b5b1a8c
songkyung <min.songkyung@gmail.com>
2025-07-31 13:16:59 +0900

    fix: SeashoreDTO 파라미터 수정

--- src/main/java/tourism_data/Surfing_The_Gangwon/dto/SeashoreDto.java
+++ src/main/java/tourism_data/Surfing_The_Gangwon/dto/SeashoreDto.java
@@ -5,18 +5,17 @@ import tourism_data.Surfing_The_Gangwon.entity.Seashore;
 
 @Builder
 public record SeashoreDto(
-    String phone,
     String name,
-    String address,
-    Integer beachCode
+    String temp, // 온도
+    String seaTemp, // 수온
+    String waveHeight, // 파고
+    String waveSpeed, // 파도속도
+    String waveDir // 파도 방향
 ) {
 
     public static SeashoreDto create(Seashore seashore) {
         return SeashoreDto.builder()
-            .phone(seashore.getPhone())
             .name(seashore.getName())
-            .address(seashore.getAddress())
-            .beachCode(seashore.getBeachCode())
             .build();
     }
 }
