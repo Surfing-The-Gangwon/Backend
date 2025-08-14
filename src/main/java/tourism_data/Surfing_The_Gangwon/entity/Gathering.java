f8851c62d9102ef9067a336b5dec13d2063fda68
unknown <kim0615157@gmail.com>
2025-08-14 16:33:39 +0900

    chore

--- src/main/java/tourism_data/Surfing_The_Gangwon/entity/Gathering.java
+++ src/main/java/tourism_data/Surfing_The_Gangwon/entity/Gathering.java
@@ -1,23 +1,19 @@
 package tourism_data.Surfing_The_Gangwon.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import tourism_data.Surfing_The_Gangwon.status.LEVEL;
@@ -32,11 +28,12 @@ public class Gathering {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User writer;
 
-    @Column(name = "seashore_id", nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "seashore_id", nullable = false)
     private Seashore seashore;
 
     @Column(name = "title", nullable = false)
